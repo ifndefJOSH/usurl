5f3ee5ffc9b021080176ec73d3a3201813cae903
Joshua Jeppson <joshua.jeppson17@gmail.com>
2023-09-14 20:04:13 -0600

    getting there

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, jsonify, Response, render_template, send_from_directory
 import sqlite3, base64, os, json, secrets
+import requests # For Google's safebrowsing API
+from urlparse import urlparse
 
-TEMPLATE_DIRECTORY = f"{os.getcwd()}/templates"
-STATIC_DIRECTORY = f"{os.getcwd()}/static"
+try:
+	RESOURCE_ROOT = os.environ["RESOURCE_ROOT"]
+except KeyError:
+	RESOURCE_ROOT = os.getcwd()
+
+TEMPLATE_DIRECTORY = f"{RESOURCE_ROOT}/templates"
+STATIC_DIRECTORY = f"{RESOURCE_ROOT}/static"
+
+try:
+	GOOGLE_SAFEBROWSING_API_KEY = os.environ["GOOGLE_SAFEBROWSING_API_KEY"]
+except KeyError:
+	print("[WARNING] Will not do safebrowsing check! Try setting the GOOGLE_SAFEBROWSING_API_KEY environment variable!")
+	GOOGLE_SAFEBROWSING_API_KEY = None
 
 app = Flask(
 	__name__
@@ -24,4 +37,32 @@ def get_client_ip():
 		return request.environ["REMOTE_ADDR"]
 	# Get the IP if behind a proxy
 	return request.environ["HTTP_X_FORWARDED_FOR"]
+
+def validate_url(url):
+	if GOOGLE_SAFEBROWSING_API_KEY is None:
+		print(f"[WARNING]: Cannot check safebrowsing for {url}!")
+		return True
+	return True # TODO
+
+@app.get("/")
+@app.get("/create")
+def get_create_page():
+	return render_template("create.html", created=False, error=False)
+
+@app.post("/create")
+def create_shortened_url():
+	# Support JSON and form
+	body = request.form
+	if not "url" in body or body["url"].strip() == "":
+		render_template("create.html", url=None, created=False, error=True, error_body="'url' field in request was empty!")
+	long_url = body["url"]
+	print(type(long_url))
+	print(f"[MESSAGE] Creating short url for '{long_url}'")
+	url = "https://kernel.org"
+	return render_template("create.html", url=url, created=True, error=False)
+
+@app.get("/l/<uid>")
+def get_redirect_page(uid):
+	url = "https://kernel.org"
+	return render_template("redirect.html", url=url)
 
